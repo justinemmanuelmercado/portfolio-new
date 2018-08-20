993f12a1c444c61406788cb69edae53d1237906a
EJ Mercado <ej@ejmercado.com>
2018-08-20 21:16:12 +0800

    final prototype

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -6,28 +6,40 @@ if (process.env.NODE_ENV !== 'production') {
 }
 
 (function () {
+  // Nodes
   const burger = document.querySelector('.burger')
   const sideNav = document.querySelector('.holygrail-nav')
   const body = document.querySelector('body')
+  const readMoreNodes = document.querySelectorAll('.read-more')
+
+  // Methods
   const toggleOpen = () => {
     sideNav.classList.toggle('open')
     body.classList.toggle('open')
-    // burger.classList.toggle('closed')
     burger.classList.toggle('open')
   }
+
   const assignActive = () => {
     const currentActive = document.querySelector('.active')
-
-    if (!location.hash) {
-      return false
-    }
 
     if (currentActive) {
       currentActive.classList.remove('active')
     }
 
-    const newActive = document.querySelector('.nav-item[href="' + location.hash + '"]')
-    newActive.classList.toggle('active')
+    if (location.hash) {
+      const newActive = document.querySelector('.nav-item[href="' + location.hash + '"]')
+      if (newActive) {
+        newActive.classList.toggle('active')
+      }
+    } else {
+      location.hash = '#about'
+      assignActive()
+    }
+  }
+
+  const readMore = (nodeName) => {
+    const toMakeOpen = document.querySelector('.project-description.' + nodeName)
+    toMakeOpen.classList.toggle('open')
   }
 
   assignActive()
@@ -40,4 +52,12 @@ if (process.env.NODE_ENV !== 'production') {
     assignActive()
     toggleOpen()
   })
+
+  for (let i = 0; i < readMoreNodes.length; i++) {
+    const currentNode = readMoreNodes[i]
+
+    currentNode.addEventListener('click', function () {
+      readMore(this.getAttribute('name'))
+    })
+  }
 })()
